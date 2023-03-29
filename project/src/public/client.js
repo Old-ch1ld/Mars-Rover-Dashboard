563cd9957d783745ae9df80ee58ea326db8df392
SonVHT <vu.hongthaison@gmail.com>
2023-03-29 22:05:18 +0700

    Completed Mars Dashboard

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,195 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+// global state
+let store = Immutable.Map({
+    rovers: Immutable.List(["Curiosity", "Opportunity", "Spirit"]),
+    showing: Immutable.List([]),
+    showTime: false,
+    loading: false,
+});
 
-// add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
+// function update global state
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+    store = store.merge(newState);
+    render(root, store);
+};
 
+// function render app html with global state
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
-
-// create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+    root.innerHTML = App(state);
+};
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+    render(root, store);
+});
 
-// ------------------------------------------------------  COMPONENTS
+// function to create app html content
+const App = (state) => {
+    const rovers = state.get("rovers").toArray();
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+    const showTime = state.get("showTime");
+
+    const showing = state.get("showing").toArray();
+
+    const loading = state.get("loading");
 
     return `
-        <h1>Hello!</h1>
-    `
-}
+        <header>
+            ${showTime ? Header(rovers) : ""}
+        </header>
+        <main>
+        ${
+            loading
+                ? Loader()
+                : showTime
+                ? ImagesShow(showing)
+                : RoverShow(rovers)
+        }
+        </main>
+        <footer></footer>
+        `;
+};
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+// function to create loader html content
+const Loader = () => {
+    return `
+    <div class="loader-box">
+        <div class="loader"></div>
+        <p class="loader-content">Please be patient my fellow Earthan, Martian photos are coming to you... </p>
+    </div>
+    `;
+};
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+// function to create header including back button & nav bar buttons, being shown on the photo gallery page
+const Header = (rovers) => {
+    return `
+    <button class="btn btn__back" onclick="EndShowTime()">Back</button>
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+    <nav class="nav-bar">
+        <button type="button" class="btn btn__${rovers[0].toLowerCase()}" onclick="showTime('Curiosity')">${
+        rovers[0]
+    }</button>
+        <button type="button" class="btn btn__${rovers[1].toLowerCase()}" onclick="showTime('Opportunity')">${
+        rovers[1]
+    }</button>
+        <button type="button" class="btn btn__${rovers[2].toLowerCase()}" onclick="showTime('Spirit')">${
+        rovers[2]
+    }</button>
+    </nav>
+    `;
+};
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+// function to create home page title & buttons
+const RoverShow = (rovers) => {
+    return `
+    <div class="rover-show">
+        <h1 class="rover-show__title">Mars Rover Show</h1>
+        ${Rovers(rovers)}
+    </div>
+    `;
+};
 
-// ------------------------------------------------------  API CALLS
+// function to create home page rover buttons
+const Rovers = (rovers) => {
+    return `
+    <div class="rover-btn-box">
+        <button class="rover-btn" title="Click to see latest images taken by ${
+            rovers[0]
+        }" type="button" onclick="showTime('Curiosity')">
+            <h3 class="rover-btn__title">${rovers[0]}</h3>
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+            <img
+                class="rover-btn__img"
+                src="./assets/images/${rovers[0].toLowerCase()}.jfif"
+                alt="${rovers[0]} Illustration"
+            />
+        </button>
+        <button class="rover-btn" title="Click to see latest images taken by ${
+            rovers[1]
+        }" type="button" onclick="showTime('Opportunity')">
+            <h3 class="rover-btn__title">${rovers[1]}</h3>
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+            <img
+                class="rover-btn__img"
+                src="./assets/images/${rovers[1].toLowerCase()}.jpg"
+                alt="${rovers[1]} Illustration"
+            />
+        </button>
+        <button class="rover-btn" title="Click to see latest images taken by ${
+            rovers[2]
+        }" type="button" onclick="showTime('Spirit')">
+            <h3 class="rover-btn__title">${rovers[2]}</h3>
 
-    return data
-}
+            <img
+                class="rover-btn__img"
+                src="./assets/images/${rovers[2].toLowerCase()}.jpg"
+                alt="${rovers[2]} Illustration"
+            />
+        </button>
+    </div>
+    `;
+};
+
+// function to create photo gallery
+const ImagesShow = (images) => {
+    // let content = "";
+    // images.forEach((i) => {
+    //     content += Image(i);
+    // });
+
+    const content = images.map((i) => Image(i)).join("\n");
+
+    return `
+    <div class="img-container">
+        ${content}
+    </div>
+    `;
+};
+
+// function to create each photo and its content
+const Image = (i) => {
+    return `
+        <div class="img-box">
+            <img
+            class="img-box__img"
+            src="${i.img_src}"
+            />
+
+            <div class="img-box__content">
+                <p>Launch Date: ${i.rover.launch_date}</p>
+                <p>Landing Date: ${i.rover.landing_date}</p>
+                <p>Status: ${i.rover.status}</p>
+                <p>Rover's Name: ${i.rover.name}</p>
+                <p>Earth Day: ${i.earth_date} </p>
+                <p>Martian Sol: ${i.sol}</p>
+                <p>Taken by: ${i.camera.name} (aka ${i.camera.full_name})</p>
+            </div>
+        </div>
+    `;
+};
+
+// function to toggle loader & load photo gallery from data returned from server
+const showTime = async (rover) => {
+    updateStore(store, { loading: true });
+
+    let images = await getRoverImages(rover);
+
+    updateStore(store, {
+        showing: Immutable.List(images),
+        showTime: true,
+        loading: false,
+    });
+};
+
+// function to go back to home page
+const EndShowTime = () => {
+    updateStore(store, { showTime: false });
+};
+
+// function to call API to get latest images from server based on rover's name
+const getRoverImages = async (rover) => {
+    const res = await (await fetch(`http://localhost:3000/${rover}`)).json();
+
+    return res.images.photos;
+};
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,28 +1,87 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use("/", express.static(path.join(__dirname, "../public")));
 
-// your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
+// function to get the latest images of Curiosity rover from nasa api
+app.get("/curiosity", async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+        const manifest = await (
+            await fetch(
+                "https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=zaVUmCw1578G1XNtShog6yqkm7fimstavCu1EEbt"
+            )
+        ).json();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+        const latestDay = manifest?.["photo_manifest"]?.["max_date"];
+
+        const images = await (
+            await fetch(
+                `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${latestDay}&api_key=${process.env.API_KEY}`
+            )
+        ).json();
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.send({ images });
+    } catch (err) {
+        console.log("error:", err);
+    }
+});
+
+// function to get the latest images of Opportunity rover from nasa api
+app.get("/opportunity", async (req, res) => {
+    try {
+        const manifest = await (
+            await fetch(
+                "https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity?api_key=zaVUmCw1578G1XNtShog6yqkm7fimstavCu1EEbt"
+            )
+        ).json();
+
+        const latestDay = manifest?.["photo_manifest"]?.["max_date"];
+
+        const images = await (
+            await fetch(
+                `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=${latestDay}&api_key=${process.env.API_KEY}`
+            )
+        ).json();
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.send({ images });
+    } catch (err) {
+        console.log("error:", err);
+    }
+});
+
+// function to get the latest images of Spirit rover from nasa api
+app.get("/spirit", async (req, res) => {
+    try {
+        const manifest = await (
+            await fetch(
+                "https://api.nasa.gov/mars-photos/api/v1/manifests/spirit?api_key=zaVUmCw1578G1XNtShog6yqkm7fimstavCu1EEbt"
+            )
+        ).json();
+
+        const latestDay = manifest?.["photo_manifest"]?.["max_date"];
+
+        const images = await (
+            await fetch(
+                `https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=${latestDay}&api_key=${process.env.API_KEY}`
+            )
+        ).json();
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.send({ images });
+    } catch (err) {
+        console.log("error:", err);
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
